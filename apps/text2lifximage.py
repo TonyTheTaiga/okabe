e277d6d0d7fd80ad6536cdb289e2eec7f8f5a5b2
Taiga Ishida <tonythetaiga@gmail.com>
2025-03-13 22:41:29 -0400

    wip

--- apps/text2lifximage.py
+++ apps/text2lifximage.py
@@ -1,4 +1,5 @@
-from okabe.nucleus import Nucleus
+from okabe import Nucleus
+from okabe.nucleus import ToolSignature
 from okabe.tools.lifx import Lifx, Light
 
 
@@ -9,15 +10,27 @@ class LightManager:
     def get_lights(self) -> list[str]:
         return [light.target for light in self.lights]
 
+    def set_light_color(self, light_id: str, color: str):
+        print(light_id, color)
+
 
 def main():
     lm = LightManager(lights=Lifx.discover())
-    nucleus = Nucleus("Get all the lifx lights in my apartment")
+    nucleus = Nucleus("Set a random LIFX bulb in my apartment to a random color")
     nucleus.add_tool_option(
         name="get_lights",
         description="Returns a list of LIFX light ids",
         callable=lm.get_lights,
         sig=[],
+    )
+    nucleus.add_tool_option(
+        name="set_light_color",
+        description="Set the color of a LIFX bulb",
+        callable=lm.set_light_color,
+        sig=[
+            ToolSignature(name="light_id", dtype="string", description="The id of the LIFX buld"),
+            ToolSignature(name="color", dtype="string", description="The color to change the light bulb to, in HEX"),
+        ],
     )
     nucleus.run()
 
